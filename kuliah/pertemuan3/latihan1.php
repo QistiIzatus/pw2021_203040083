989caf8646d8d23c421d056ccd923531e43f9e77
QistiIzatus <qistiizatussyabani8910@gmail.com>
2021-03-13 20:45:44 +0700

    pertemuan3

--- kuliah/pertemuan3/latihan1.php
+++ kuliah/pertemuan3/latihan1.php
@@ -1,73 +1,62 @@
 <?php
 /*
-Qisti Izatussyabani
-203040083
-https://github.com/QistiIzatus/pw2021_203040050
-Pertemuan 3 - (18 februari 2021)
-Mempelajari mengenai struktur kendali pada php yaitu pengulangan dan pengkondisian
+    Qisti Izatussyabani
+    203040083
+    https://github.com/QistiIzatus/pw2021_203040083
+    Pertemuan 3 
+    Materi minggu ini mempelajari mengenai Control flow/Struktur kendali
 */
 ?>
 
-<?php
+<?php 
 // pengulangan
-
-// for
-for( $i = 0; $i < 5; $i++ ) {
-    echo "Hello World! <br>";
-}
-
+// for 
 // while
-$i = 0;
-while ( $i < 5 ) {
-    echo "Hello HafizaSaidatll! <br>";
-    $i++;
-}
-
-// do.. while
-$i = 0;
-do {
-    echo "Hello Fiza! <br>";
-    $i++;
-} while( $i <5 );
-
-
-
-
+// do.. while 
 // foreach : pengulangan khusus array
 
+// for( $i = 0; $i < 5; $i++ ) {
+//      echo "Hello World! <br>";
+// }
+
+// $i = 10;
+// while( $i < 5 ) {
+//     echo "Hello World! <br>";
+// $i++;
+// }
+// $i = 10;
+// do {
+//     echo "Hello World! <br>";
+// $i++;
+// } while( $i < 5 );
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Latihan 1 html</title>
+    <title>Latihan 1</title>
     <style>
         .warna-baris {
-        background-color : yellow;
-
-    }
+            background-color: silver;
+        }
     </style>
 </head>
 <body>
+    
 
-
-<table border="1" cellpadding="10" cellspacing="0">
-<?php for( $i = 1; $i <= 5; $i++ ) : ?>
-<?php if($i % 2== 1 ) : ?>
-
-<tr class="warna-baris">
-<?php else : ?>
-<tr>
-<?php endif; ?>
-<?php for( $j = 1; $j <= 5; $j++ ) : ?>
-<td><?= "$i, $j"; ?></td>
-<?php endfor; ?>
-</tr>
-<?php endfor; ?>
-
-
-</table>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php for( $i = 1; $i <= 5; $i++ ) : ?>
+            <?php if( $i % 2 == 1 ) : ?>
+                <tr class="warna-baris">
+            <?php else : ?>
+            <tr>
+            <?php endif; ?>
+            <?php for( $j = 1; $j <= 5; $j++ ) : ?>
+                <td><?= "$i, $j"; ?></td>
+            <?php endfor; ?>
+        </tr>
+        <?php endfor; ?>
+    </table>
 </body>
-
 </html>
