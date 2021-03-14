1447f1c98c345be9d2729a8c55037bc0bc8df275
QistiIzatus <qistiizatussyabani8910@gmail.com>
2021-03-14 22:53:51 +0700

    pertemuan5

---
+++ kuliah/pertemuan5/latihan1.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Qisti Izatussyabani
+203040083
+https://github.com/QistiIzatus/pw2021_203040083
+Pertemuan 5
+*/
+?>
+
+<?php
+// array
+// variabel yang dapat memiliki banyak nilai 
+// elemen pada array boleh memiliki tipe data yang berbeda
+// pasangan anatara key dan value
+// key-nya adalah index, yang dimulai dari 0 
+
+// membuat array
+// cara lama
+$hari = array("Senin", "Selasa", "rabu");
+// cara baru
+$bulan = ["januari", "Februari", "Maret"];
+$arr1 = [123, "tulisan", false];
+
+// menampilkan Array
+// var_dump() / print_r()
+// var_dump($hari);
+// echo "<br>";
+// print_r($bulan);
+//echo "<br>";
+
+// Menampilkan 1 elemen pada array
+// echo $arr1[0];
+// echo "<br>";
+// echo $bulan[1]
+
+// menambahkan elemen baru pada array
+var_dump($hari);
+$hari[] = "Kamis";
+$hari[] = "Jum'at";
+echo "<br>";
+var_dump($hari);
+
+
+
+
+?>
---
+++ kuliah/pertemuan5/latihan3.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Qisti Izatussyabani
+203040083
+https://github.com/QistiIzatus/pw2021_203040083
+Pertemuan 5
+*/
+?>
+
+<?php  
+$mahasiswa = [
+	["Qisti Izatussyabani", "203040083", "Teknik nformatika", "qistiizatussyabani8910@gmail.com"], ["Gania Rahma", "203040070", "Teknik nformatika", "ganiarahma123@gmail.com"]
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Daftar Mahasiswa</title>
+</head>
+<body>
+	<h1>Daftar Mahasiswa</h1>
+
+	<?php foreach ($mahasiswa as $mhs) {?>
+	
+	<ul>
+		<li>Nama : <?php echo $mhs[0]; ?></li>
+		<li>NRP : <?php echo $mhs[1]; ?></li>
+		<li>Jurusan : <?php echo $mhs[2]; ?></li>
+		<li>Email : <?php echo $mhs[3]; ?></li>
+	</ul>
+<?php } ?>
+</body>
+</html>
+
