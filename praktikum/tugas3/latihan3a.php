3b8db840799ab19e6300560baa084cf9d0962310
QistiIzatus <qistiizatussyabani8910@gmail.com>
2021-03-14 23:29:39 +0700

    ...

--- praktikum/tugas3/latihan3a.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Qisti Izatussyabani
-// 203040083
-// Shift Jumat 08:00 - 09:00
-
-?><?php 
-$sambung = ["ada", "abel", "men", "pung", "nilai"];
-echo "Array $sambung[0]lah suatu vari$sambung[1] yang dapat $sambung[2]am$sambung[3] banyak $sambung[4]";
-?>
