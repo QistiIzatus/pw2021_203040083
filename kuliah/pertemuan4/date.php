d01dc7bf44b8baad55a9b9da928f9b53399e0949
QistiIzatus <qistiizatussyabani8910@gmail.com>
2021-03-14 21:40:23 +0700

    pertemuan4

--- kuliah/pertemuan4/date.php
+++ kuliah/pertemuan4/date.php
@@ -1,59 +1,45 @@
-
 <?php
 /*
 Qisti Izatussyabani
 203040083
-github.com/QistiIzatus/pw2021_203040083
+https://github.com/QistiIzatus/pw2021_203040083
 Pertemuan 4 
-Materi pertemuan ke-4 ini mempelajari penggunaan function pada PHP (Date/Time)
+Mempelajari mengenai Function
 */
 ?>
 
 <?php
-//DATE 
-// Untuk menampilkan tanggal dengan format tertentu 
+// Date
+// Untuk Menampilkan Tanggal dengan formattertentu
 
-echo date("l");
-echo date("d");
-echo date("M");
-echo date("m");
-echo date("l, d-M-Y"); 
+    //  echo date("l, d-M-Y");
 
-//TIME
-// UNIX Timestamp / EPOCH Time
-// detik yang sudah berlalu sejak 1 Januari 1970
 
-echo time(); 
-echo date("l", time()+172800);                  //menghitung 2 hari kedepan
-echo date("l", time()+60*60*24*100);            //menghitung 100 hari kedepan 
-echo date("d M Y", time()-60*60*24*100);        // 100 hari kebelakang
+//  Time 
+//  UNIX Timestamp / EPOCH time
+// Detik yang sudah berlalu sejak 1 januari 1970 
 
-// MKTIME
-// membuat detik sendiri 
-// mktime (0,0,0,0,0,0)
-// jam, menit, detik, bulan, tanggal, tahun
+     //  echo time();
+     // echo date("l", time()-60*60*24*100);
 
-echo mktime(0,0,0,7,14,2002);
-echo date("l", mktime(0,0,0,7,14,2002));
+
+//  mktime
+//  membuat sendiri detik
+//  mktime(0,0,0,0,0,0)
+//  jam,menit,detik,bulan,tanggal, tahun
+echo date ("l", mktime(0,0,0,22,11,1967));
 
 // strtotime
-// kebalikan dari mktime
-
-echo strtotime("14 jul 2002");
-echo date("l", strtotime("14 jul 2002"));
-
-/* function yang sering dipakai pada seri PHP 
-yang berhubungna dengan String:
-1. strlen() = menghitung panjang sebuah String
-2. strcmp() = untuk membandingkan 2 buah String 
-3. explode() = memecah sebuah String menjadi Array 
-4. htmlspecialchars() = function khusus untuk menjaga kita dari hacker 
-Utility:
-1. var_dump() = untuk mencetak sebuah objek, array
-2. isset() = utk mengecek apakah sebuah variabel sudah pernah dibikin atau belum akan menampilkan boolean
-3. empty() = utk mengecek apakah variabel yang kita buat itu ada isinya atau tidak 
-4. die() = untuk memberhentikan program kita 
-5. sleep() = utk memberhentikan sementara
-*/ 
-
+// echo date("l", strtotime("25 aug 1985"));
 ?>
+<!-- STRING
+strlen() untuk menghitung panjang sebuah string
+strcmp() untuk membandingkan 2 buah string
+explode() untuk memecah sebuah string menjadi array
+htmlspecialchars untuk menjaga website kita dari hacker
+UTILITY 
+var_dump() untuk mencetak isi sebuah variabel, objek, array
+isset() untuk mengecek apakah sebuah variabel suda dibuat apa belum, dan akan menghasilan boolean true or false
+empty() untuk mengecek apakah sebuah variabel ada isi nya atau tidak
+die() ketika program bertemu die makan program dibawahnya tidak akan dieksekusi
+sleep() untuk memberhentikan sementara -->
